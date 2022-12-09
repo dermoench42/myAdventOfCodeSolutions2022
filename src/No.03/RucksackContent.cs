b59ff058cfead88ea92d93a209532f8c33700f2d
Ervin Peters <ervin.peters@ervnet.de>
2022-12-09 06:40:46 +0100

    wip no 3,1

--- src/No.03/RucksackContent.cs
+++ src/No.03/RucksackContent.cs
@@ -7,20 +7,32 @@ namespace No._03
 {
     public class RucksackContent
     {
-        private List<char> itemsFound;
+        private readonly List<char> itemsFound = new();
+        private readonly List<char> itemsDone = new();
 
         public RucksackContent(string content)
         {
             int compartmentLength = content.Length / 2;
 
-            string c0 = content[..compartmentLength];
+            string c0 = content[..(compartmentLength)];
             string c1 = content[compartmentLength..];
 
+            foreach (char item in c0.ToArray())
+            {
+                if (this.itemsDone.Contains(item))
+                    continue;
 
+                if (c1.Contains(item))
+                    this.itemsFound.Add(item);
 
+                this.itemsDone.Add(item);
+            }
         }
 
         public int calcPrio()
-            => this.itemsFound.Sum(item => item);
+            => this.itemsFound
+                .Sum(item => item >= 'a'
+                                    ? item - 'a' + 1
+                                    : item - 'A' + 27);
     }
 }
