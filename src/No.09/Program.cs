1ab036d4bce2ae4b41c8c8ba129bcc08bb9f4063
Ervin Peters <coder@ervnet.de>
2022-12-15 07:25:10 +0100

    created no9 projects

---
+++ src/No.09/Program.cs
@@ -0,0 +1,12 @@
+﻿// (c) 2022 QSOFT Development
+
+namespace No._09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ src/No.09_Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+// (c) 2022 QSOFT Development
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace No._09_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
